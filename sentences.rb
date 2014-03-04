25f6f8e06288825ec1735faf348bf4fde053c7f0
SundayJive <twelveletter@yandex.ru>
2014-03-04 09:01:55 +0200

    sentences.rb

--- sentences.rb
+++ sentences.rb
@@ -1,3 +1,10 @@
+#http://www.codewars.com/dojo/katas/52dffa05467ee54b93000712
+#Sort sentence pseudo-alphabetically:
+#Lowercase words first, sorted alphabetically
+#Uppercase words last, in reverse alphabetical order
+#Punctuation is omitted
+#Example:
+#sort_sentence('A BB c, a b. C') ==> 'a b c C BB A'
 def sort sentence
   sentence = sentence.gsub(/[^A-Za-z\s]/, '').split(' ')
   left = sentence.select {|x| x[0] == x[0].downcase}
